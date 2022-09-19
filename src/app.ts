056e0448129bbd3f166588f9c9fba3d9155f8203
Keny Travanino <keny03@unitec.edu>
2022-09-18 19:32:54 -0700

    Adding morgan for helper

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import morgan from 'morgan';
 import config from './config';
 import campusRoutes from './routes/Campus.Routes';
 
 const app = express();
 app.set('port', config.MONGO_PORT);
+
+app.use(morgan('dev'));
 
 app.use(campusRoutes);
 export default app;
